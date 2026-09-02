8eadfd4d56b54385713a9d036a37d0e7906f93e6
benedictatac <atanaciocharles@yahoo.com>
2026-09-02 17:02:46 -0400

    [version_1.0_0.1]
    - corrected wrong host permissions content

---
+++ word-lookup/sidebar/sidebar.js
@@ -0,0 +1,16 @@
+
+//Following steps are required for minimal functionality 
+/*
+Step 1: Search for the word 
+Step 2: Word lookup -> API calls to Dictionary, in case of emergency -> will go into fallback
+Step 3: Rendering the results to the user 
+Step 4: Submit button -> submits to notion database -> Notion API called 
+*/
+
+
+
+
+async function fetchWordFromDict(string)
+{
+
+}
